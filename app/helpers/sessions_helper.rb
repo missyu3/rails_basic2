dd2cf555a415ef8e1efceb04c73240453d00a505
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-03-23 11:58:35 +0900

    レイアウトの設定を作成

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,14 @@ module SessionsHelper
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
+  def current_user_content?(content_user)
+    if content_user == current_user
+      true
+    else
+      false
+    end
+  end
+
   def logged_in?
     current_user.present?
   end
